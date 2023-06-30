092adb16a7760e0f1cf939cec74813c1955cb74d
Hafeezur2001 <116695659+Hafeezur2001@users.noreply.github.com>
2023-06-30 12:58:51 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -3,6 +3,11 @@ public class Question1
   public static void main(String[] args)
   {
     System.out.println("Hello World!");
-    
+
+    /*
+     * Print "Hello World!"
+     * 
+     * Hint: System.out.println(...)
+     */
   }
 }
